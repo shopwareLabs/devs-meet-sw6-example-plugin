82c0f0289c824623855d71a80a741b3e445466a4
Christian Rades <c.rades@shopware.com>
2019-11-12 15:42:47 +0100

    only use single canvas in n storefront plugin

--- src/Resources/storefront/personal-product-viewer/personal-product-viewer.plugin.js
+++ src/Resources/storefront/personal-product-viewer/personal-product-viewer.plugin.js
@@ -1,40 +1,46 @@
 import Plugin from 'src/script/plugin-system/plugin.class';
 
 export default class PersonalProductViewer extends Plugin {
-    static options={
+    static options = {
         x0: 0.0,
         y0: 0.0,
         x1: 10,
         y1: 10,
+        baseImage: null,
     };
 
     init() {
         const canvas = this.el.querySelector('[data-personal-overlay]');
         console.log(canvas);
         const ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'rgba(255, 255, 0, 0.9)';
-        ctx.fillRect(
-            this.options.x0,
-            this.options.y0,
-            this.options.x1 - this.options.y0,
-            this.options.y1 - this.options.y0,
-        );
+        const baseImg = new Image();
+        baseImg.onload = () => {
+            ctx.fillStyle = 'rgba(200, 20, 100, 0.9)';
+            ctx.drawImage(baseImg,0, 0);
+            ctx.fillRect(
+                this.options.x0,
+                this.options.y0,
+                this.options.x1 - this.options.x0,
+                this.options.y1 - this.options.y0,
+            );
 
-        this.urlInput = document.querySelector('input[name=\'personal-product-unsplash-url\']');
+            this.urlInput = document.querySelector('input[name=\'personal-product-unsplash-url\']');
 
-        this.urlInput.oninput = (ev) => {
-            const img = new Image();
-            img.onload = () => {
-                ctx.drawImage(
-                    img,
-                    this.options.x0,
-                    this.options.y0,
-                    this.options.x1 - this.options.y0,
-                    this.options.y1 - this.options.y0,
-                );
-            };
-            console.log(ev);
-            img.src=ev.target.value;
+            this.urlInput.oninput = (ev) => {
+                const img = new Image();
+                img.onload = () => {
+                    ctx.drawImage(
+                        img,
+                        this.options.x0,
+                        this.options.y0,
+                        this.options.x1 - this.options.x0,
+                        this.options.y1 - this.options.y0,
+                    );
+                };
+                console.log(ev);
+                img.src = ev.target.value;
+            }
         }
+        baseImg.src = this.options.baseImage;
     }
 }
