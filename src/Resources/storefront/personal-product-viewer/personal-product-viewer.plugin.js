1a256d49a03a438ab36f90b1e29d77fd46206bd1
Christian Rades <c.rades@shopware.com>
2019-11-12 14:10:37 +0100

    fixed properties in storefront plugin

--- src/Resources/storefront/personal-product-viewer/personal-product-viewer.plugin.js
+++ src/Resources/storefront/personal-product-viewer/personal-product-viewer.plugin.js
@@ -2,10 +2,10 @@ import Plugin from 'src/script/plugin-system/plugin.class';
 
 export default class PersonalProductViewer extends Plugin {
     static options={
-        x: 0.0,
-        y: 0.0,
-        width: 10,
-        height: 10,
+        x0: 0.0,
+        y0: 0.0,
+        x1: 10,
+        y1: 10,
     };
 
     init() {
@@ -14,10 +14,10 @@ export default class PersonalProductViewer extends Plugin {
         const ctx = canvas.getContext('2d');
         ctx.fillStyle = 'rgba(255, 255, 0, 0.9)';
         ctx.fillRect(
-            this.options.x,
-            this.options.y,
-            this.options.width,
-            this.options.height
+            this.options.x0,
+            this.options.y0,
+            this.options.x1 - this.options.y0,
+            this.options.y1 - this.options.y0,
         );
 
         this.urlInput = document.querySelector('input[name=\'personal-product-unsplash-url\']');
@@ -27,10 +27,10 @@ export default class PersonalProductViewer extends Plugin {
             img.onload = () => {
                 ctx.drawImage(
                     img,
-                    this.options.x,
-                    this.options.y,
-                    this.options.width,
-                    this.options.height
+                    this.options.x0,
+                    this.options.y0,
+                    this.options.x1 - this.options.y0,
+                    this.options.y1 - this.options.y0,
                 );
             };
             console.log(ev);
