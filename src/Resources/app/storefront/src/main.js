8875bb8492e804b00e47d37ea4dcfdb78acd2f2d
Timo Altholtmann <t.altholtmann@shopware.com>
2019-12-05 14:42:14 +0100

    example02

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,5 +1,5 @@
 /** Import JavaScript plugin classes */
-import PersonalProductViewer from './js/personal-product-viewer.plugin';
+
 import ImageChanger from './js/image-changer.plugin';
 
 /** Register plugins in the plugin manager */
